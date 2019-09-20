d5ca4584ff0d3affe007b58022bb4daa95651599
Gustavo Coronel <gcoronelc@gmail.com>
2019-09-19 22:02:25 -0500

    Clase 02

--- Clase02/PedidoApp/src/pe/uni/pedidoapp/PedidoApp.java
+++ Clase02/PedidoApp/src/pe/uni/pedidoapp/PedidoApp.java
@@ -1,9 +1,13 @@
 package pe.uni.pedidoapp;
 
+import pe.uni.pedidoapp.view.PedidoView;
+
 public class PedidoApp {
 
 	public static void main(String[] args) {
 		System.out.println("Aun no tengo nada programado!!!!");
+		PedidoView view = new PedidoView();
+		view.setVisible(true);
 	}
 	
 }
